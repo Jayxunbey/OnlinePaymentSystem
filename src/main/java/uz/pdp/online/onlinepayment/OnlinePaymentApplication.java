822443735123c62e0f3fe3e28dce4e9d00ec74c8
Jayxunbey <muxammedovjayxun@gmail.com>
2024-04-29 17:01:22 +0500

    first saved

---
+++ src/main/java/uz/pdp/online/onlinepayment/OnlinePaymentApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.online.onlinepayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinePaymentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/online/onlinepayment/OnlinePaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.online.onlinepayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinePaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
