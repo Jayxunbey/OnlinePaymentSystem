ba442d070c61b683b6a996a8b4f1e6a2c7b40c4c
Jayxunbey <67492124+Jayxun@users.noreply.github.com>
2024-05-03 00:55:05 +0500

    Update OnlinePaymentSystemApplication.java
    Signed-off-by: Jayxunbey <67492124+Jayxun@users.noreply.github.com>

--- src/main/java/uz/pdp/online/onlinepayment/OnlinePaymentSystemApplication.java
+++ src/main/java/uz/pdp/online/onlinepayment/OnlinePaymentSystemApplication.java
@@ -8,7 +8,7 @@ public class OnlinePaymentSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OnlinePaymentSystemApplication.class, args);
-        jk
+        jk,
     }
 
 }
