330f3a99f2c2609befae8c55dc88d93094e052b0
Jayxunbey <muxammedovjayxun@gmail.com>
2024-05-09 19:04:17 +0500

    added basic secure

--- src/main/java/uz/pdp/online/onlinepayment/config/SecurityConfig.java
+++ src/main/java/uz/pdp/online/onlinepayment/config/SecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
-    private String[] WHITE_LIST = {
+    private final String[] WHITE_LIST = {
             "/api/test/**",
     };
 
