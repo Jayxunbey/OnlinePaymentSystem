ef5bb842c9b89282489733e6073c139b9b453d45
Jayxunbey <muxammedovjayxun@gmail.com>
2024-05-15 00:38:53 +0500

    added succesfully

--- src/main/java/uz/pdp/online/onlinepayment/entity/inpostgres/Region.java
+++ src/main/java/uz/pdp/online/onlinepayment/entity/inpostgres/Region.java
@@ -7,15 +7,15 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "region")
+@Table
 public class Region {
     @Id
-    @Column(name = "id", nullable = false)
+    @Column(nullable = false)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "region_id_gen")
     @SequenceGenerator(name = "region_id_gen", sequenceName = "region_id_seq", allocationSize = 1)
     private Integer id;
 
-    @Column(name = "name", nullable = false)
+    @Column(nullable = false)
     private String name;
 
 }
