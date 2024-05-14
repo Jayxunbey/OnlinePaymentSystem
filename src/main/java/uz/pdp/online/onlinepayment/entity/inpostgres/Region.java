40da120486818bb1b645672196b988c7685f4bee
Jayxunbey <muxammedovjayxun@gmail.com>
2024-05-15 00:37:44 +0500

    added succesfully

--- src/main/java/uz/pdp/online/onlinepayment/entity/inpostgres/Region.java
+++ src/main/java/uz/pdp/online/onlinepayment/entity/inpostgres/Region.java
@@ -1,12 +1,8 @@
 package uz.pdp.online.onlinepayment.entity.inpostgres;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.ColumnDefault;
 
 @Getter
 @Setter
@@ -14,8 +10,9 @@ import org.hibernate.annotations.ColumnDefault;
 @Table(name = "region")
 public class Region {
     @Id
-    @ColumnDefault("nextval('region_id_seq'::regclass)")
     @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "region_id_gen")
+    @SequenceGenerator(name = "region_id_gen", sequenceName = "region_id_seq", allocationSize = 1)
     private Integer id;
 
     @Column(name = "name", nullable = false)
