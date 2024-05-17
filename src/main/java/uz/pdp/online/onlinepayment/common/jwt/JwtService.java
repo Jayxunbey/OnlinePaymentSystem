ada9e346ba3e9a88143e458c66af2fe456cbd9e2
Jayxunbey <muxammedovjayxun@gmail.com>
2024-05-17 10:21:48 +0500

    added jwt Service with some methods

--- src/main/java/uz/pdp/online/onlinepayment/common/jwt/JwtService.java
+++ src/main/java/uz/pdp/online/onlinepayment/common/jwt/JwtService.java
@@ -1,10 +1,57 @@
 package uz.pdp.online.onlinepayment.common.jwt;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class JwtService {
 
+    @Value("${spring.security.jwt.login-remember-me-expiration-time-minute}")
+    private int expirationWithRememberMeTimeMinute;
 
+    @Value("${spring.security.jwt.login-without-remember-me-expiration-time-minute}")
+    private int expirationWithOutRememberMeTimeMinute;
+
+    @Value("${spring.security.jwt.secret-key}")
+    private String SECRET_KEY;
+
+    public String generateToken(String username, boolean rememberMe) {
+        return generateToken(username, new HashMap<>(), rememberMe);
+    }
+
+    public String generateToken(String username, Map<String, Object> claims, boolean rememberMe) {
+
+        int expiration;
+        if (rememberMe) {
+            expiration = expirationWithRememberMeTimeMinute;
+        } else {
+            expiration = expirationWithOutRememberMeTimeMinute;
+        }
+        return buildToken(username, claims, expiration);
+    }
+
+    private String buildToken(String username, Map<String, Object> claims, int expiration) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + (long) expiration * 60 * 1000))
+                .signWith(getSignKey(), SignatureAlgorithm.ES256)
+                .compact();
+    }
+
+    private SecretKey getSignKey() {
+        byte[] decode = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(decode);
+    }
 
 }
