6465a86e8d30ed9ea5e60cf1ef202b922fe9d0a0
Jayxunbey <muxammedovjayxun@gmail.com>
2024-06-03 15:51:54 +0500

    added example of inner dto to dto

--- src/main/java/uz/pdp/online/onlinepayment/entity/inmongo/Field.java
+++ src/main/java/uz/pdp/online/onlinepayment/entity/inmongo/Field.java
@@ -2,6 +2,8 @@ package uz.pdp.online.onlinepayment.entity.inmongo;
 
 import lombok.*;
 
+import java.util.List;
+
 @Builder
 @Getter
 @Setter
@@ -9,6 +11,7 @@ import lombok.*;
 @NoArgsConstructor
 @ToString
 public class Field {
+    private List<FieldCaptionsOnLanguages> fieldCaptionsOnLanguages;
     private String name;
     private String type;
     private boolean required;
